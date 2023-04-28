6000ccfdd9e76d1e6e86ff8c03ac5b8f30a75ef1
Airiell <airiellgarrett@gmail.com>
2023-04-28 17:19:47 -0500

    damnFork

---
+++ iWishApp/Controllers/UserLoginController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using iWishApp.Models;
+
+namespace iWishApp.Controllers;
+
+public class UserLoginController : Controller
+{
+    private readonly ILogger<UserLoginController> _logger;
+
+    public UserLoginController(ILogger<UserLoginController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ iWishApp/Models/UserLogin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace iWishApp.Models
+{
+    public class UserLogin
+    { 
+        public int Id { get; set; } 
+        public string UserName { get; set; }
+        public string Password { get; set; }    
+        public string Email { get; set; }
+
+        public List<Affirmations> Favorites { get; set; }
+
+        public UserLogin(string username, string password, string email) 
+        { 
+            UserName = username;
+            Password = password;
+            Email = email;
+        }
+    }
+}
