2724cc6aca2f59d28dd8e1f4499cd09d2b1ac661
Briaris Sayles <bri.sayles2@gmail.com>
2023-05-05 11:37:22 -0500

    added Affirmations model from another branch

--- iWishApp/Models/Affirmations.cs
+++ iWishApp/Models/Affirmations.cs
@@ -1,11 +1,35 @@
 ﻿using System;
 namespace iWishApp.Models
 {
-	public class Affirmations
-	{
-		public Affirmations()
-		{
-		}
-	}
-}
+    public class Affirmations
+    {
+        public class Affirmations
+        {
+            public int Id { get; set; }
+            static private int nextId = 1;
 
+            [Required(ErrorMessage = "The name of the affirmation is required.")]
+            public string Name { get; set; }
+            public string Category { get; set; }
+            public string Text { get; set; }
+            //public DateTime DateCreated { get; set; }
+
+
+
+
+            public Affirmations()
+            {
+
+            }
+
+            public Affirmations(string name, string category, string text) : this()
+            {
+                Name = name;
+                Category = category;
+                Text = text;
+            }
+        }
+
+    }
+
+}
