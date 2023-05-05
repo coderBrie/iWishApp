4331ab0730d121b60aebab604af8b96a358dba5e
Briaris Sayles <bri.sayles2@gmail.com>
2023-05-05 11:43:48 -0500

    added Name, Category , and Text to view model

--- iWishApp/ViewModels/AddAffirmationsViewModel.cs
+++ iWishApp/ViewModels/AddAffirmationsViewModel.cs
@@ -3,9 +3,15 @@ namespace iWishApp.ViewModels
 {
 	public class AddAffirmationsViewModel
 	{
-		public AddAffirmationsViewModel()
-		{
-		}
-	}
+
+        [Required(ErrorMessage = "The name of the affirmation is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The category of the affirmation is required.")]
+        public string Category { get; set; }
+
+        [Required(ErrorMessage = "The text of the affirmation is required.")]
+        public string Text { get; set; }
+    }
 }
 
