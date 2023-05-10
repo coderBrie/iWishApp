2e68c353adde9fb4a083cef197be0c3c759d0e3d
Briaris Sayles <bri.sayles2@gmail.com>
2023-05-10 11:23:48 -0500

    added blog entry view model

--- iWishApp/Models/BlegEntry.cs
+++ iWishApp/Models/BlegEntry.cs
@@ -7,6 +7,7 @@ namespace iWishApp.Models
         public string Title { get; set; }
         public string Content { get; set; }
         public DateTime CreatedAt { get; set; }
+       
     }
 }
 
---
+++ iWishApp/ViewModels/BlogEntryViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace iWishApp.ViewModels
+{
+    public class BlogEntryViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Display(Name = "Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DatePosted { get; set; }
+
+        public string Category { get; set; }
+
+        public List<string> Tags { get; set; }
+    }
+}
+
